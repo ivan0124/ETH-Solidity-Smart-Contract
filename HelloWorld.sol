0e7ab81ca2f16626ac5ab4b38b7eb2d9ed69f3bf
ivan <ivan0124.tw@gmail.com>
2021-08-14 22:59:13 +0800

    add initial smart contract
    add initial smart contract

---
+++ HelloWorld.sol
@@ -0,0 +1,57 @@
+pragma solidity 0.4.22;
+contract HelloWorld {
+
+ address owner;
+ string greetStr = "hello world";
+ constructor() public {
+ owner = msg.sender;
+ }
+ 
+  function greet() external view returns (address) {
+     if (msg.sender == owner) {
+         //return strConcat(greetStr, " ", "boss111");
+         return owner;
+     } 
+  }
+  
+  function balance() external view returns (uint256) {
+     
+         return owner.balance;
+    
+  }
+
+ /*
+ function greet() public constant returns (string) {
+     if (msg.sender == owner) {
+         //return strConcat(greetStr, " ", "boss111");
+         return "111";
+     } 
+ }
+ */
+ 
+/*
+event LogInt(string, int);
+ function log(string s , int x) internal {
+    emit LogInt(s, x);
+}
+*/
+
+/*
+ function strConcat(string _a, string _b, string _c) private pure returns
+(string){
+ bytes memory _ba = bytes(_a);
+ bytes memory _bb = bytes(_b);
+ bytes memory _bc = bytes(_c);
+
+ string memory abcde = new string(_ba.length + _bb.length + _bc.length);
+ bytes memory babcde = bytes(abcde);
+
+ uint k = 0;
+ for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
+ for (i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
+ for (i = 0; i < _bc.length; i++) babcde[k++] = _bc[i]; 
+
+ return string(babcde);
+ }
+*/
+} 
