d6800a6b57d8f6ea3c5779707fdc5eca0bff8a98
ivan <ivan0124.tw@gmail.com>
2021-08-15 14:40:03 +0800

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -8,6 +8,20 @@ contract HelloWorld {
  }
  
  /****************************/
+    struct Book { 
+      string title;
+      string author;
+      uint book_id;
+   }
+   Book book;
+
+   function setBook() public {
+      book = Book('Learn Java', 'TP', 199);
+   }
+   function getBookId() public view returns (uint) {
+      return book.book_id;
+   }
+ /****************************/
  uint256[] public numbers;// 動態長度陣列
  function addNumber(uint256 _number) external {
           numbers.push(_number);
