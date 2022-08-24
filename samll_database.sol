c1ded25a2bcf711901a9fb5b575fa7e271fc3445
ivan <ivan0124.tw@gmail.com>
2022-08-24 13:42:49 +0800

    Update small_database.sol
    Update small_database.sol

--- samll_database.sol
+++ samll_database.sol
@@ -1,18 +1,26 @@
-pragma solidity 0.4.22;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+/*
+interface data_base {
+    function add_user(string memory _user, string memory _userdata) external;
+    function get_user(string memory _user) external view returns (string memory);
+}
+*/
+
 contract small_database {
 
-
- constructor() public {
+ //constructor() public {
   //owner = msg.sender;
- }
+ //}
  
  mapping(string => string) database;
  
- function add_user(string _user, string _userdata) external {
+ function add_user(string memory _user, string memory _userdata) external {
      database[_user]=_userdata;
  }
  
- function get_user(string _user) external view returns (string){
+ function get_user(string memory _user) external view returns ( string memory data){
      return  database[_user];
  }
  
