47fb19198eb8d97722c032c6684332b0a4354301
ivan <ivan0124.tw@gmail.com>
2022-08-24 13:43:56 +0800

    Update EtherWallet.sol
    Update EtherWallet.sol

--- EtherWallet.sol
+++ EtherWallet.sol
@@ -2,8 +2,14 @@
 
 pragma solidity = 0.8.11;
 
+interface data_base {
+    function add_user(string memory _user, string memory _userdata) external;
+    function get_user(string memory _user) external view returns (string memory);
+}
+
 contract EtherWallet{
     address payable public owner;
+    data_base d = data_base(0x13CE26383e9cD3a7b8dD66874c47c5d8293c2239); //add small_database address
 
     constructor(){
         owner = payable(msg.sender);
@@ -19,4 +25,12 @@ contract EtherWallet{
     function getBalance() external view returns (uint) {
         return address(this).balance;
     }
+
+     function add(string memory _user, string memory _userdata) external{
+         d.add_user(_user,  _userdata);
+     }
+
+     function get(string memory _user) external view returns ( string memory data){
+         return  d.get_user(_user);
+     }
 }
